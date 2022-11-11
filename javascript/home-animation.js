c9b1eb1babff45b963d25bdfb29bf7b85f1841e3
Kim TaeHyeong <kysth0707@naver.com>
2022-11-11 19:56:37 +0900

    home animation update

--- javascript/home-animation.js
+++ javascript/home-animation.js
@@ -13,14 +13,54 @@ window.onload = function()
 		});
 	});
 	
-	
-	// let Contents = document.querySelectorAll('.home-animation-content');
 	let Contents = document.getElementsByClassName('home-animation-content');
-	// Contents.array.forEach(element => {
-	// 	Observer.observe(element);
-	// });
 	for(let i = 0; i < Contents.length; i++)
 	{
 		Observer.observe(Contents[i]);
+	}
+
+
+
+
+
+	let Observer2 = new IntersectionObserver((e)=>{
+		e.forEach((박스)=>{
+			if(박스.isIntersecting)
+			{
+				박스.target.style.left = "-20%";
+			}
+			else
+			{
+				박스.target.style.left = "0%";
+			}
+		});
+	});
+	
+	let Contents2 = document.getElementsByClassName('home-animation-left-content');
+	for(let i = 0; i < Contents2.length; i++)
+	{
+		Observer2.observe(Contents2[i]);
+	}
+
+
+
+
+	let Observer3 = new IntersectionObserver((e)=>{
+		e.forEach((박스)=>{
+			if(박스.isIntersecting)
+			{
+				박스.target.style.left = "20%";
+			}
+			else
+			{
+				박스.target.style.left = "0%";
+			}
+		});
+	});
+	
+	let Contents3 = document.getElementsByClassName('home-animation-right-content');
+	for(let i = 0; i < Contents3.length; i++)
+	{
+		Observer3.observe(Contents3[i]);
 	}
 }
